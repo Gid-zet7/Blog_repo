c25c3deb6f6e41dcdcf8cbd7ebb1450761729136
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-03-25 15:45:09 +0000

    Add seeds for blogs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Blog.create(title: "first seed blog", body: "When I count my blisters, I count blessings")
+Blog.create(title: "Second seed blog", body: "When I count my blisters, I dodge drama like neo")
